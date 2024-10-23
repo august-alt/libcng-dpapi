8b4b681438c814ec353abd924c7e97be1b075568
kiper220-alt <gedertkj@basealt.ru>
2024-10-23 17:09:19 +0400

    wip: implement reading encrypted data from AD

--- src/blob.c
+++ src/blob.c
@@ -28,11 +28,61 @@
 #include <gnutls/crypto.h>
 #include <gnutls/abstract.h>
 
+#include <openssl/kdf.h>
+#include <openssl/params.h>
+#include <openssl/core_names.h>
+#include <openssl/evp.h>
+
 #include "pkcs7_p.h"
 #include "pkcs7/KEKRecipientInfo.h"
 
+
 #define CONTENT_TYPE_ENVELOPED_DATA_OID "1.2.840.113549.1.7.3"
 #define MAX_BUFFER_SIZE 16384
+
+// TODO: to refine napilnikom
+struct ProtectionDescriptor*
+unpack_protection_descriptor_sid(
+        TALLOC_CTX *mem_ctx,
+        KEKRecipientInfo_t *kekInfo
+)
+{
+    // TODO: Add to ASN.1 ProtectionDescriptor.
+    // TODO: Add function for OIDs(validation too).
+    // 1.3.6.1.4.1.311.74.1 - MICROSOFT_SOFTWARE_OID
+
+/*
+    reader = ASN1Reader(data).read_sequence()
+    content_type = reader.read_object_identifier()
+
+    reader = reader.read_sequence().read_sequence().read_sequence()
+    value_type = reader.read_utf8_string()
+    value = reader.read_utf8_string()
+*/
+
+    // TEMPORARY
+    if (!mem_ctx)
+    {
+        printf("%s:%s:%d Passed talloc_ctx is NULL. Error = 0x%x (%s)\n",
+               __FILE__, __func__, __LINE__, 0, "TALLOC_CTX is NULL!");
+
+        return NULL;
+    }
+
+    struct ProtectionDescriptor* descriptor = NULL;
+
+    if (kekInfo->kekid.other->keyAttr->size <= 27 || 
+        strncmp(&kekInfo->kekid.other->keyAttr->buf[22], "SID", 3) != 0)
+    {
+        return NULL;
+    }
+
+    descriptor = talloc(mem_ctx, struct ProtectionDescriptor);
+    descriptor->value = strndup(&kekInfo->kekid.other->keyAttr->buf[27], kekInfo->kekid.other->keyAttr->size - 27);
+    descriptor->type = SID;
+
+    return descriptor;
+}
 
 blob_t *
 blob_unpack(
@@ -84,8 +134,8 @@ blob_unpack(
         0x01, 0x00, 0x04, 0x80, 0x54, 0x00, 0x00, 0x00, 0x60, 0x00, 0x00, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x14, 0x00, 0x00, 0x00, 0x02, 0x00, 0x40, 0x00, 0x02, 0x00, 0x00, 0x00,
         0x00, 0x00, 0x24, 0x00, 0x03, 0x00, 0x00, 0x00, 0x01, 0x05, 0x00, 0x00, 0x00, 0x00,
-        0x00, 0x05, 0x15, 0x00, 0x00, 0x00, 0x08, 0x70, 0x66, 0x99, 0x73, 0xf4, 0xc7, 0xf5,
-        0x08, 0x6e, 0x25, 0x31, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x02, 0x00,
+        0x00, 0x05, 0x15, 0x00, 0x00, 0x00, 0xCC, 0x48, 0x40, 0x3E, 0x05, 0xFD, 0xE0, 0x01,
+        0x57, 0xE4, 0x28, 0xB4, 0x00, 0x02, 0x00, 0x00, 0x00, 0x00, 0x14, 0x00, 0x02, 0x00,
         0x00, 0x00, 0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
         0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x12, 0x00, 0x00, 0x00, 0x01, 0x01,
         0x00, 0x00, 0x00, 0x00, 0x00, 0x05, 0x12, 0x00, 0x00, 0x00
@@ -137,7 +187,54 @@ compute_kdf(const uint8_t  *algorithm,
             uint32_t length,
             uint8_t **out)
 {
-    return -1;
+    EVP_KDF *kdf;
+    EVP_KDF_CTX *kctx;
+    int result = 0;
+    int KBKDF_use_l = 0;
+    char* _algorithm = strdup(algorithm);
+    OSSL_PARAM params[6], *p = params;
+
+    kdf = EVP_KDF_fetch(NULL, "KBKDF", NULL);
+    kctx = EVP_KDF_CTX_new(kdf);
+    EVP_KDF_free(kdf);
+
+    *p++ = OSSL_PARAM_construct_utf8_string(OSSL_KDF_PARAM_DIGEST,
+                                            _algorithm, strlen(_algorithm));
+    *p++ = OSSL_PARAM_construct_utf8_string(OSSL_KDF_PARAM_MAC,
+                                            "HMAC", strlen("HMAC"));
+    *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_KEY,
+                                            secret, strlen(secret));
+    *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_SALT,
+                                            label, strlen(label));
+    *p++ = OSSL_PARAM_construct_octet_string(OSSL_KDF_PARAM_INFO,
+                                            context, strlen(context));
+    // *p++ = OSSL_PARAM_construct_int(OSSL_KDF_PARAM_KBKDF_USE_L,
+    //                                          &KBKDF_use_l);
+    *p = OSSL_PARAM_construct_end();
+
+    if (EVP_KDF_derive(kctx, *out, length, params) <= 0)
+    {
+        result = -1;
+    }
+
+
+    // if (result == 0) {
+    //     printf("%s'0x%02x'", "grep \"[", (*out)[0]);
+    //     bool firstOutput = true;
+    //     for (int i = 1; i < length; ++i)
+    //     {
+    //         if(firstOutput)
+    //         {
+    //             printf(", '0x%02x'", (*out)[i]);
+    //         }
+    //     }
+    //     printf("%s", "]\"");
+    // }
+
+    EVP_KDF_CTX_free(kctx);
+    free(_algorithm);
+
+    return result;
 }
 
 static uint32_t
@@ -173,10 +270,10 @@ get_kek(GroupKeyEnvelope *key_envelope, struct KeyEnvelope *key_identifier, gnut
         return -1;
     }
 
-    uint8_t *l2_key = NULL;
-    uint32_t l2_key_size = 0;
-    uint8_t *KDS_SERVICE_LABEL = "";
-    uint32_t KDS_SERVICE_LABEL_SIZE = 0;
+    uint8_t *l2_key = key_envelope->l2_key;
+    uint32_t l2_key_size = key_envelope->l2_key_len;
+    uint8_t *KDS_SERVICE_LABEL = "KDS service";
+    uint32_t KDS_SERVICE_LABEL_SIZE = strlen(KDS_SERVICE_LABEL);
 
     if (false)
     {
@@ -190,6 +287,8 @@ get_kek(GroupKeyEnvelope *key_envelope, struct KeyEnvelope *key_identifier, gnut
 
         uint32_t status = 0;
         kek->size = 32;
+        kek->data = malloc(32);     // Rewrite with talloc
+
         status = compute_kdf(kdf_parameters.hash_algorithm,
                              l2_key,
                              l2_key_size,
@@ -300,13 +399,13 @@ unpack_response(TALLOC_CTX *mem_ctx,
         return -1;
     }
 
-    GroupKeyEnvelope *result = talloc_zero(mem_ctx, GroupKeyEnvelope);
+    GroupKeyEnvelope *group_key_envelope = talloc_zero(mem_ctx, GroupKeyEnvelope);
     enum ndr_err_code ndr_status = NDR_ERR_SUCCESS;
 
     DATA_BLOB data_blob;
     data_blob.data = (uint8_t*)data;
     data_blob.length = size;
-    ndr_status = ndr_pull_struct_blob(&data_blob, mem_ctx, result, (ndr_pull_flags_fn_t)ndr_pull_GroupKeyEnvelope);
+    ndr_status = ndr_pull_struct_blob(&data_blob, mem_ctx, group_key_envelope, (ndr_pull_flags_fn_t)ndr_pull_GroupKeyEnvelope);
     if (ndr_status != NDR_ERR_SUCCESS)
     {
         printf("%s:%s:%d Failed to decode GroupKeyEnvelope object. Error = 0x%x (%s)\n",
@@ -318,7 +417,7 @@ unpack_response(TALLOC_CTX *mem_ctx,
     gnutls_datum_t kek = { 0 };
     int rc = 0;
 
-    rc = get_kek(result, &initial_data->key_identifier, &kek);
+    rc = get_kek(group_key_envelope, &initial_data->key_identifier, &kek);
     if (rc != 0)
     {
         printf("%s:%s:%d Failed to get key encryption key. Error = 0x%x (%s)\n",
